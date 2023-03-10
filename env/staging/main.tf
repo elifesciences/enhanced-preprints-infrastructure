4f8001309f720c21c226c5bfc3c887adc195f892
Scott Aubrey <scott@aubrey.org.uk>
2023-03-10 15:37:07 +0000

    add ability for developers to become biorxiv s3 bucket xml access role

--- env/staging/main.tf
+++ env/staging/main.tf
@@ -6,4 +6,5 @@ module "epp" {
   irsa_oidc_provider    = "oidc.eks.us-east-1.amazonaws.com/id/0108D0073AFB87B6669E378F0A9CFB76"
   irsa_namespace        = "epp--staging"
   irsa_service_account  = "default"
+  biorxiv_role_arn      = "arn:aws:iam::512686554592:role/access-biorxiv-xml"
 }
