82f2f3816576f2cbace37db2624369de54ecfcc8
Scott Aubrey <scott@aubrey.org.uk>
2023-05-19 16:10:49 +0100

    add readonly role

--- modules/epp/iam_roles.tf
+++ modules/epp/iam_roles.tf
@@ -28,6 +28,35 @@ resource "aws_iam_role_policy_attachment" "read-write-role-policy-attachment" {
 }
 
 
+resource "aws_iam_role" "read_only_role" {
+  name = "epp-${var.env}-s3-read-only"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Effect : "Allow",
+        Principal : {
+          Federated : "arn:aws:iam::${var.account_id}:oidc-provider/${var.irsa_oidc_provider}"
+        },
+        Condition : {
+          StringEquals : {
+            "${var.irsa_oidc_provider}:sub" : "system:serviceaccount:${var.read_only_irsa_namespace}:${var.read_only_irsa_service_account}"
+          }
+        }
+      },
+    ]
+  })
+
+  tags = local.tags
+}
+resource "aws_iam_role_policy_attachment" "read-write-role-policy-attachment" {
+  role       = aws_iam_role.read_only_role.name
+  policy_arn = aws_iam_policy.read_only.arn
+}
+
+
 resource "aws_iam_role" "import_role" {
   name = "epp-${var.env}-import"
 
