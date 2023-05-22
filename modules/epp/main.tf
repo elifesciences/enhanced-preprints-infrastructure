62f03f5ccd965af15aa9d3289d02874fab0d6395
Scott Aubrey <scott@aubrey.org.uk>
2023-05-22 16:48:56 +0100

    add in the resources for other buckets

--- modules/epp/main.tf
+++ modules/epp/main.tf
@@ -1,6 +1,8 @@
 locals {
   project_name = "epp"
-  bucket_name  = "${var.env}-elife-epp-data"
+  main_bucket_name  = "${var.env}-elife-epp-data"
+  meca_bucket_name  = "${var.env}-elife-epp-meca"
+  pdf_bucket_name  = "${var.env}-elife-epp-pdf"
   tags = {
     Project     = local.project_name
     Environment = var.env
@@ -8,6 +10,16 @@ locals {
 }
 
 resource "aws_s3_bucket" "main_bucket" {
-  bucket = local.bucket_name
+  bucket = local.main_bucket_name
+  tags   = local.tags
+}
+
+resource "aws_s3_bucket" "meca_bucket" {
+  bucket = local.meca_bucket_name
+  tags   = local.tags
+}
+
+resource "aws_s3_bucket" "pdf_bucket" {
+  bucket = local.pdf_bucket_name
   tags   = local.tags
 }
